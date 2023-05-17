98c1929264c6094177bd252ea7b438a1283b643e
impronen <impronen@gmail.com>
2023-05-17 14:16:56 +0300

    additional configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Page Title',
+      title: 'Battleship',
       template: './src/index.html',
     }),
   ],
