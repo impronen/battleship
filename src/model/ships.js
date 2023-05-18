34e3dc45764611b81b40002d691ffe0c1508ad20
impronen <impronen@gmail.com>
2023-05-18 12:47:15 +0300

    small change

--- src/model/ships.js
+++ src/model/ships.js
@@ -4,6 +4,7 @@ export class ship {
     this.type = type;
     this.health = this.getSquares();
   }
+
   getSquares() {
     if (this.type === 'carrier') return 5;
     else if (this.type === 'battleship') return 4;
@@ -11,17 +12,23 @@ export class ship {
     else if (this.type === 'submarine') return 3;
     else if (this.type === 'destroyer') return 2;
   }
+
   getPlayer() {
     return this.player;
+  }
+  getType() {
+    return this.type;
   }
 
   getHealth() {
     return this.health;
   }
+
   hit() {
     if (this.health >= 1) this.health = this.health - 1;
     return this.health;
   }
+
   isSunk() {
     return this.health === 0;
   }
