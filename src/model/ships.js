865e09f3f1272f24b1e6474981e8d02c67383740
impronen <impronen@gmail.com>
2023-05-18 11:25:54 +0300

    ship class + ship tests Add tests for ship class and build class. Tests pass

--- src/model/ships.js
+++ src/model/ships.js
@@ -1,7 +1,28 @@
-/* class ship {
-    constructor(player, type)
-} */
+export class ship {
+  constructor(player, type) {
+    this.player = player;
+    this.type = type;
+    this.health = this.getSquares();
+  }
+  getSquares() {
+    if (this.type === 'carrier') return 5;
+    else if (this.type === 'battleship') return 4;
+    else if (this.type === 'cruiser') return 3;
+    else if (this.type === 'submarine') return 3;
+    else return 2;
+  }
+  getPlayer() {
+    return this.player;
+  }
 
-export function testFunction(a) {
-  return a;
+  getHealth() {
+    return this.health;
+  }
+  hit() {
+    this.health = this.health - 1;
+    return this.health;
+  }
+  isSunk() {
+    return this.health === 0;
+  }
 }
--- src/tests/ships.test.js
+++ src/tests/ships.test.js
@@ -1,5 +1,32 @@
-import { testFunction } from '../model/ships';
+import { ship } from '../model/ships';
 
-test('this is a test test', () => {
-  expect(testFunction('A')).toBe('A');
+describe('Ship functionality tests', () => {
+  test('test that ship returns the correct number of squares', () => {
+    const newShip = new ship('antero', 'submarine');
+    expect(newShip.getSquares()).toBe(3);
+  });
+
+  test('test that ship returns the correct name', () => {
+    const newShip = new ship('antero', 'submarine');
+    expect(newShip.getPlayer()).toBe('antero');
+  });
+
+  test('test that ship returns correct health', () => {
+    const newShip = new ship('antero', 'submarine');
+    expect(newShip.getHealth()).toBe(3);
+  });
+
+  test('test that ship health is impacted when it is hit', () => {
+    const newShip = new ship('antero', 'submarine');
+    newShip.hit();
+    expect(newShip.getHealth()).toBe(2);
+  });
+
+  test('test that ship is sunk, when health goes to zero', () => {
+    const newShip = new ship('antero', 'submarine');
+    newShip.hit();
+    newShip.hit();
+    newShip.hit();
+    expect(newShip.isSunk()).toBe(true);
+  });
 });
