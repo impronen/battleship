9cf96de98e840b4be096faa470b8f63813923d80
impronen <impronen@gmail.com>
2023-05-22 16:29:48 +0300

    add initial gameboard tests + code

--- src/model/gameboard.js
+++ src/model/gameboard.js
@@ -1,15 +1,52 @@
+/* import { ship } from './ships'; */
+
 export class gameBoard {
   constructor(player) {
     this.player = player;
-    this.sunkShips = 0;
+    this._sunkShips = 0;
+    this._board = Array(10).fill(Array(10).fill(null));
   }
+  // Basic properties - how many sunk ships / have all been sunk
   shipWasSunk() {
-    this.sunkShips = this.sunkShips + 1;
+    this._sunkShips = this._sunkShips + 1;
   }
   getSunkShips() {
-    return this.sunkShips;
+    return this._sunkShips;
   }
   isAllLost() {
-    return this.sunkShips === 5;
+    return this._sunkShips === 5;
+  }
+
+  // Methods relating to gameboard - the content of square etc
+  getSquareContent(x, y) {
+    return this._board[x][y];
+  }
+  setSquareContent(x, y, content) {
+    this._board[x][y] = content;
+  }
+
+  hitSquare(x, y) {
+    if (this.getSquareContent(x, y) === 'hit') return false;
+    else {
+      this._board[x][y] = 'hit';
+    }
+  }
+
+  // Ships - placement, checking for possible collisions
+  placeShip([x, y], orientation, ship) {
+    let length = ship.getSquares();
+    if (orientation === 'horizontal') {
+      while (length > 0) {
+        this.setSquareContent(x, y, ship.getType());
+        x++;
+        length--;
+      }
+    } else {
+      while (length > 0) {
+        this.setSquareContent(x, y, ship.getType());
+        y--;
+        length--;
+      }
+    }
   }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,6 +1,13 @@
 import { gameBoard } from '../model/gameboard';
+import { ship } from '../model/ships';
 
 describe('Gameboard: testing functionality for sinking ships', () => {
+  test('knows the number of sunk ships on the board', () => {
+    const newBoard = new gameBoard();
+    newBoard.shipWasSunk();
+    expect(newBoard.getSunkShips()).toBe(1);
+  });
+
   test('knows if all ships on the board been sunk', () => {
     const newBoard = new gameBoard();
     newBoard.shipWasSunk();
@@ -10,16 +17,47 @@ describe('Gameboard: testing functionality for sinking ships', () => {
     newBoard.shipWasSunk();
     expect(newBoard.isAllLost()).toBe(true);
   });
+});
 
-  test('knows the number of sunk ships on the board', () => {
+describe('Gameboard: tests for hitting a square and what did it contain', () => {
+  test('detects that the hit is succesful on an empty square', () => {
     const newBoard = new gameBoard();
-    newBoard.shipWasSunk();
-    expect(newBoard.getSunkShips()).toBe(1);
+    newBoard.hitSquare(1, 5);
+    expect(newBoard.getSquareContent(1, 5)).toBe('hit');
+  });
+
+  test('does not allow you to hit a square that is already hit', () => {
+    const newBoard = new gameBoard();
+    newBoard.hitSquare(1, 5);
+    expect(newBoard.hitSquare(1, 5)).toBe(false);
   });
 });
 
-/* describe('Gameboard: tests for has a square been hit and what did it contain', () => {});
+describe('Gameboard: tests for ship placement', () => {
+  test('test ship placed horizontally is in the grid (square 1)', () => {
+    const newBoard = new gameBoard();
+    const battleship = new ship('antero', 'battleship');
+    newBoard.placeShip([1, 5], 'horizontal', battleship);
+    expect(newBoard.getSquareContent(1, 5)).toBe('battleship');
+  });
+  test('test ship placed horizontally is in the grid (square 4)', () => {
+    const newBoard = new gameBoard();
+    const battleship = new ship('antero', 'battleship');
+    newBoard.placeShip([1, 5], 'horizontal', battleship);
+    expect(newBoard.getSquareContent(5, 5)).toBe('battleship');
+  });
+  test('test ship placed horizontally is in the grid (square 1)', () => {
+    const newBoard = new gameBoard();
+    const battleship = new ship('antero', 'battleship');
+    newBoard.placeShip([2, 5], 'vertical', battleship);
+    expect(newBoard.getSquareContent(2, 5)).toBe('battleship');
+  });
+  test('test ship placed horizontally is in the grid (square 1)', () => {
+    const newBoard = new gameBoard();
+    const battleship = new ship('antero', 'battleship');
+    newBoard.placeShip([2, 5], 'vertical', battleship);
+    expect(newBoard.getSquareContent(2, 2)).toBe('battleship');
+  });
+});
 
-describe('Gameboard: tests for ship placement', () => {});
-
-describe('Gameboard: tests for ship receiving an attack', () => {}); */
+/* describe('Gameboard: tests for ship receiving an attack', () => {}); */
