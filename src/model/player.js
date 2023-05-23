b3d83674c4118dd0dd14eb68df376150ca775fa4
impronen <impronen@gmail.com>
2023-05-23 19:02:07 +0300

    getRandomCoordinates() checks for duplicates

--- src/model/player.js
+++ src/model/player.js
@@ -3,6 +3,43 @@ export class Player {
     this.Player = name;
     this.type = type;
     this.ships = [];
-    this.shots = {};
+    this.shots = [];
+  }
+  getType() {
+    return this.type;
+  }
+
+  saveShot([x, y]) {
+    this.shots.push([x, y]);
+  }
+  checkShots() {
+    return this.shots;
+  }
+
+  // To be used by the AI player for ship placement and shooty things
+  getRandomCoordinates() {
+    let coords = [];
+    let i = 2;
+    while (i > 0) {
+      let random = Math.random() * (7 - 0) + 0;
+      coords.push(Math.round(random));
+      i--;
+    }
+    // Recursive case to check if shots array already contains the generated number
+    if (
+      this.shots.some((shotSquare) =>
+        // For some weird reason the coords will not find a match with contents of shots
+        // array unless there is a digit by digit check (something about deep equality?)
+        shotSquare.every((digit, index) => digit === coords[index])
+      )
+    ) {
+      return this.getRandomCoordinates();
+    }
+    this.shots.push(coords);
+  }
+  getRandomOrientation() {
+    let random = Math.random() * (10 - 1) + 1;
+    if (random >= 5) return 'vertical';
+    return 'horizontal';
   }
 }
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,1 +1,36 @@
 import { Player } from '../model/player';
+
+describe('Player: tests for things related to shots', () => {
+  test('Shots array is returned as it should be', () => {
+    const newPlayer = new Player('antero', 'human');
+    newPlayer.saveShot([1, 1]);
+    newPlayer.saveShot([5, 3]);
+    expect(newPlayer.checkShots()).toStrictEqual([
+      [1, 1],
+      [5, 3],
+    ]);
+  });
+});
+
+describe('Player: AI functionality', () => {
+  test('Random coordinates are produced', () => {
+    const newPlayer = new Player('BOB', 'AI');
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    newPlayer.getRandomCoordinates();
+    expect(newPlayer.checkShots().length).toBe(16);
+  });
+});
