398ab8ea6f58322fa955ec8f27eb14319cc9b48d
impronen <impronen@gmail.com>
2023-05-24 15:55:19 +0300

    tests + method for changing turns

--- src/controller/game.js
+++ src/controller/game.js
@@ -2,16 +2,30 @@ import { Player } from '../model/player';
 import { gameBoard } from '../model/gameboard';
 
 export class Game {
-  constructor(player1, player2, gameboard1, gameboard2) {
+  constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
-    this.gameboard1 = gameboard1;
-    this.gameboard2 = gameboard2;
+    this.currentPlayer = this.player1.getName();
   }
+
   getPlayer1Stats() {
     return [this.player1.getType(), this.player1.getName()];
   }
   getPlayer2Stats() {
     return [this.player2.getType(), this.player2.getName()];
+  }
+  getCurrentPlayer() {
+    return this.currentPlayer;
+  }
+  changeTurn(hit = false) {
+    let human = this.player1.getName();
+    let ai = this.player2.getName();
+    if (hit === true) return;
+    console.log(this.currentPlayer);
+    if (this.currentPlayer === human) {
+      this.currentPlayer = ai;
+    } else {
+      this.currentPlayer = human;
+    }
   }
 }
--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -19,3 +19,23 @@ describe('Game: all class instances are created', () => {
   test('Human players gameboard is generated', () => {});
   test('AI players gameboard is generated', () => {});
 });
+
+describe('Game: turns are changed appropriately', () => {
+  test('turn is changed if a no ship is hit HUMAN => AI', () => {
+    const newGame = new Game(
+      new Player('antero', 'human'),
+      new Player('Bob', 'AI')
+    );
+    newGame.changeTurn();
+    expect(newGame.getCurrentPlayer()).toBe('Bob');
+  });
+  test('turn is changed if a no ship is hit AI => HUMAN', () => {
+    const newGame = new Game(
+      new Player('antero', 'human'),
+      new Player('Bob', 'AI')
+    );
+    newGame.changeTurn();
+    newGame.changeTurn();
+    expect(newGame.getCurrentPlayer()).toBe('antero');
+  });
+});
