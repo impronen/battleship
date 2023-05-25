04a760bb3ebd9438d4734ffe2a9153824f49aba2
impronen <impronen@gmail.com>
2023-05-25 14:05:57 +0300

    add html/css basics + js imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('TEE');
+import './style.css';
+import { dom } from './view/dom';
+import { Game } from './controller/game';
+import { Gameboard } from './model/gameboard';
+import { Player } from './model/player';
---
+++ src/view/dom.js
@@ -0,0 +1,2 @@
+import { Game } from '../controller/game';
+export const dom = () => {};
