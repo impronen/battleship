2822391ddc23d0cdde967afae952569fe2df0935
impronen <impronen@gmail.com>
2023-05-26 12:39:36 +0300

    grid creation + style

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import { dom } from './view/dom';
 import { Game } from './controller/game';
 import { Gameboard } from './model/gameboard';
 import { Player } from './model/player';
+import { events } from './view/eventlisteners';
 
 dom.createGameboards();
+events.startListener();
+events.orientationListener();
--- src/view/dom.js
+++ src/view/dom.js
@@ -1,11 +1,15 @@
 import { Game } from '../controller/game';
+import { events } from './eventlisteners';
 export const dom = (() => {
   function createGrid(board) {
     for (let i = 0; i <= 9; i++) {
+      let column = document.createElement('div');
+      column.classList.add('column');
+      board.appendChild(column);
       for (let l = 0; l <= 9; l++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
-        board.appendChild(cell);
+        column.appendChild(cell);
       }
     }
   }
@@ -13,9 +17,7 @@ export const dom = (() => {
   function createGameboards() {
     const humangameboard = document.querySelector('.humangameboard');
     const aigameboard = document.querySelector('.aigameboard');
-
     createGrid(humangameboard);
-
     createGrid(aigameboard);
   }
 
